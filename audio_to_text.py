04af267e42b92ca943d8e54242943ba5673e49f1
Yash-KK <reachyashk@gmail.com>
2025-07-30 15:05:44 +0530

    generate text output (feedback)

---
+++ audio_to_text.py
@@ -0,0 +1,39 @@
+import assemblyai as aai
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
+aai.settings.api_key = os.getenv("SETTINGS_API_KEY")
+
+
+
+class AssemblyAudioTranscription:
+    def transcribe_audio(self, audio_file):
+        config = aai.TranscriptionConfig(speaker_labels=True)
+        try:
+            transcript = aai.Transcriber().transcribe(audio_file, config)
+            return transcript
+        except Exception as e:
+            print("Error: ", e)
+            return None
+
+    def df_conversion(self, result):
+        if result == None: 
+            print("result is None")
+            return
+        
+        try:
+            segments = []
+            for utterance in result.utterances:
+                segments.append({
+                    "start": utterance.start / 1000,  # ms to seconds
+                    "end": utterance.end / 1000,
+                    "text": utterance.text,
+                    "speaker": utterance.speaker
+                })
+            
+            df = pd.DataFrame(segments)
+            return df
+        except Exception as e:
+            print("Error when converting to DataFrame: ",e)
+            return None
